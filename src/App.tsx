b2e5415ac67a59bcc7624396eef8129582a64782
victoria tapia <tapia.victoria5070@gmail.com>
2025-07-08 20:24:37 -0700

    add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
 
 function App() {
     return (
         <>
             <HeroSection />
+            <AboutSection />
         </>
     );
 }
--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Button } from 'twinkle-kit/core';
-import { Icon } from './ui/Icon';
 
 import './styles.css';
 
 const AboutSection: React.FC = () => {
     return (
         <section
-            className='about-section bg-primary-3'
+            className='about-section bg-neutral-1'
             role='banner'
-            aria-labelledby='about-heading'
+            aria-labelledby='about-section'
         >
-            <h1 id='about-heading' className='text-accent'>
+            <h2 id='about-section' className='text-accent'>
                 About Me
-            </h1>
+            </h2>
 
-            <p aria-describedby='about-heading' className=''>
-                Building thoughtful software and empowering the people behind
-                it.
+            <p aria-describedby='about-section' className=''>
+                I’m a full-stack engineer and tech lead with 8+ years of
+                experience shipping resilient systems across fintech,
+                healthcare, e-commerce, and developer platforms, and 3+ years in
+                technical leadership roles. I work across the stack, with a
+                focus on frontend architecture, clean backend integrations, and
+                product-driven code.
+            </p>
+            <p aria-describedby='about-section' className=''>
+                I care deeply about creating calm, high-functioning engineering
+                cultures. Whether I’m prototyping a 0–1 product, contributing to
+                launches, or re-architecting legacy systems, I aim to bring
+                curiosity, clarity, and care to both code and collaboration.
             </p>
         </section>
     );

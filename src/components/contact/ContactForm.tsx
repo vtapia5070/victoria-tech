5ef048a51d0f6bbab3389c9b59d3d799ee7da131
victoria tapia <tapia.victoria5070@gmail.com>
2025-08-06 13:47:22 -0700

    add contact form with placehodler submit

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,22 +1,56 @@
-import { useRef, type FC } from 'react';
-import { Button } from 'twinkle-kit';
+import { type FC, useState } from 'react';
+import { Button, Input } from 'twinkle-kit';
+
+interface ContactFormData {
+    firstName: string;
+    email: string;
+}
+
+const usePostContact = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState<null | string>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const postContact = async (contactBody: ContactFormData) => {
+        setIsLoading(true);
+        try {
+            const res = await fetch('some-url', {
+                method: 'POST',
+                body: JSON.stringify(contactBody),
+            });
+            const responseData = await res.json();
+            setData(responseData);
+        } catch (err) {
+            setError(err instanceof Error ? err.message : 'An error occurred');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { data, error, isLoading, postContact };
+};
 
 export const ContactForm: FC = () => {
-    const inputRef = useRef(null);
-    const handleSubmit = (formValues) => {
-        console.log('submit', formValues);
-        console.log('firstName', formValues.get('firstName'));
+    const { postContact, isLoading } = usePostContact();
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        console.log('firstName', formData.get('firstName'));
+        console.log('email', formData.get('email'));
+        postContact({
+            firstName: formData.get('firstName') as string,
+            email: formData.get('email') as string,
+        });
     };
     return (
-        <form action={handleSubmit}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor='first-name'>First Name:</label>
-            <input
-                type='text'
-                id='first-name'
-                ref={inputRef}
-                name='firstName'
-            />
-            <Button type='submit'>Submit</Button>
+            <Input name='firstName' />
+            <Input inputId='email' name='email' />
+            <Button type='submit' isLoading={isLoading}>
+                Submit
+            </Button>
         </form>
     );
 };
